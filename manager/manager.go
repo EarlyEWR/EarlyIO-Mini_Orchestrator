4e6c3ebe7ffdf9f912962190f289ea1cdde1520a
EarlyEWR <evan@earlyref.com>
2025-10-23 20:32:21 +0900

    Initial commit: EarlyIO Mini Orchestrator

---
+++ manager/manager.go
@@ -0,0 +1,29 @@
+package manager
+
+import (
+	"EarlyIO-Mini_Orchestrator/task"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Manager struct {
+	Pending       queue.queue
+	TaskDb        map[string][]*task.TaskDb
+	EventDb       map[string][]*task.taskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
+}
